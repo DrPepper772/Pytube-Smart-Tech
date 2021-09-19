3e79fab1c9630a2ad67165252134a2b8102d9cbb
Quack*2 <89680161+Quack-2@users.noreply.github.com>
2021-09-19 10:49:47 +0800

    Update Pytube-Smart Tech.py

--- Pytube-Smart Tech.py
+++ Pytube-Smart Tech.py
@@ -3,22 +3,21 @@ from pytube import YouTube
 from pytube.cli import on_progress
 import os
 
-
 Invalid_url = True
 Invalid_command = True
-#use while loop to make sure url is correct
+#use while loop to make sure url is correct format
 #if url cant be readed by pytube, type in the url again
 while Invalid_url:
     try:
         URL = str(input("Enter the URL of the video you want to download: \n>> "))
         yt =YouTube(URL, on_progress_callback=on_progress)
         Invalid_url = False
-    #url not string or cant be detected by pytube 
+    #url not string or cant be detected by pytube
     except:
         print('Invalid URL. Please try again')
         
-print("Select download format:")
 #add show valid command for user
+print("Select download format:")
 print("1: Video file with audio (.mp4)")
 print("2: Audio only (.mp3)")
 
@@ -30,35 +29,33 @@ while Invalid_command:
         if media_type == "1":
             #setting for mp4
             video = yt.streams.get_by_resolution("720p")
-            #count file size 
+            #count file size
             filesize=round(video.filesize/(1024*1024))
             Invalid_command = False
 
         elif media_type == "2":
             #setting for mp3
             video = yt.streams.filter(only_audio = True).first()
-            #count file size 
-            print('FileSize : ' + str(round(video.filesize/(1024*1024))) + 'MB')
+            #count file size
+            filesize=round(video.filesize/(1024*1024))
             Invalid_command = False
             
         #if command not equal to '1' / '2' run again
         else:
-            print('Invalid command. Please Try again')
-    #if command not in int type, run again
+           print('Invalid command. Please Try again')
     except:
         print('Invalid Command. Please Try again')
-
-##print("Enter the destination (leave blank for current directory)")
-#download video 
-out_file = video.download(output_path = os.path.expanduser("~\Downloads"))
+print("Loading....")
+#download video
+out_file = video.download(output_path = os.path.expanduser("~\Downloads")) #~given path to user’s home directory
 
 if media_type == "2":
-    base, ext = os.path.splitext(out_file)
+    base, ext = os.path.splitext(out_file)#split the path name into a pair root and ext
     new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+    os.rename(out_file, new_file)#turns mp4 to mp3 audio file
     
-#show size file 
-print('FileSize : ' + str() + 'MB')
+#show size file
+print('FileSize : ' + str(filesize) + 'MB')
 #remind when video download success
 print(yt.title + " has been successfully downloaded.")
 #show file that download video
